658b06d35b9e6e1101b52f2a33d56d3ee8180948
Konstantin <konstantin.muenster@mail.com>
2023-04-29 11:05:00 +0200

    calculating menu position

--- src/components/Editor/plugins/FloatingMenuPlugin.tsx
+++ src/components/Editor/plugins/FloatingMenuPlugin.tsx
@@ -1,8 +1,16 @@
+import { computePosition } from "@floating-ui/dom";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
-import { useRef, useState } from "react";
+import {
+  $getSelection,
+  $isRangeSelection,
+  COMMAND_PRIORITY_NORMAL,
+  SELECTION_CHANGE_COMMAND,
+} from "lexical";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { createPortal } from "react-dom";
 
 import { FloatingMenu, FloatingMenuCoords } from "../components/FloatingMenu";
+import { usePointerInteractions } from "../hooks/usePointerInteractions";
 
 const DOM_ELEMENT = document.body;
 
@@ -10,6 +18,65 @@ export function FloatingMenuPlugin() {
   const ref = useRef<HTMLDivElement>(null);
   const [coords, setCoords] = useState<FloatingMenuCoords>(undefined);
   const [editor] = useLexicalComposerContext();
+
+  const { isPointerDown, isPointerReleased } = usePointerInteractions();
+
+  const calculatePosition = useCallback(() => {
+    const domSelection = getSelection();
+    const domRange =
+      domSelection?.rangeCount !== 0 && domSelection?.getRangeAt(0);
+
+    if (!domRange || !ref.current || isPointerDown) return setCoords(undefined);
+
+    computePosition(domRange, ref.current, { placement: "top" })
+      .then((pos) => {
+        setCoords({ x: pos.x, y: pos.y - 10 });
+      })
+      .catch(() => {
+        setCoords(undefined);
+      });
+  }, [isPointerDown]);
+
+  const $handleSelectionChange = useCallback(() => {
+    if (editor.isComposing()) return false;
+
+    if (editor.getRootElement() !== document.activeElement) {
+      setCoords(undefined);
+      return true;
+    }
+
+    const selection = $getSelection();
+
+    if ($isRangeSelection(selection) && !selection.anchor.is(selection.focus)) {
+      calculatePosition();
+    } else {
+      setCoords(undefined);
+    }
+
+    return true;
+  }, [editor, calculatePosition]);
+
+  useEffect(() => {
+    const unregisterCommand = editor.registerCommand(
+      SELECTION_CHANGE_COMMAND,
+      $handleSelectionChange,
+      COMMAND_PRIORITY_NORMAL
+    );
+    return unregisterCommand;
+  }, [editor, $handleSelectionChange]);
+
+  const show = coords !== undefined;
+
+  useEffect(() => {
+    if (!show && isPointerReleased) {
+      editor.getEditorState().read(() => {
+        $handleSelectionChange();
+      });
+    }
+    // Adding show to the dependency array causes an issue if
+    // a range selection is dismissed by navigating via arrow keys.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isPointerReleased, $handleSelectionChange, editor]);
 
   return createPortal(
     <FloatingMenu ref={ref} editor={editor} coords={coords} />,
